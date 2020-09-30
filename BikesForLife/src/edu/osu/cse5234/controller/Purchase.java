b1779391d784928bc45768a13bff0139ec75170a
Eli Vosniak <eli.vosniak@gmail.com>
2020-09-30 19:11:17 -0400

    Added some methods to controller class

--- BikesForLife/src/edu/osu/cse5234/controller/Purchase.java
+++ BikesForLife/src/edu/osu/cse5234/controller/Purchase.java
@@ -4,11 +4,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.osu.cse5234.model.*;
 @Controller
 @RequestMapping("/purchase")
 public class Purchase {
 	
+	@RequestMapping(method = RequestMethod.GET)
+	public String viewOrderEntryForm(HttpServletRequest request, HttpServletResponse response) {
+		// ... instantiate and set order object with items to display
+		return "OrderEntryForm";
+	}
+	
+	@RequestMapping(path = "/submitItems", method = RequestMethod.POST)
+	public String submitItems(@ModelAttribute("order") Order order, HttpServletRequest request) {
+		request.getSession().setAttribute("order", order);
+		return "redirect:/purchase/paymentEntry";
+	}
 }
